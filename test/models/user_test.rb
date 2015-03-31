ba63949ff613399fbd1715cef15d1387954392ed
Zmago Devetak <zmago_devetak@yahoo.com>
2015-03-31 12:28:32 +0000

    Add user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,6 +7,14 @@ class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
+  
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 
   test "should be valid" do
     assert @user.valid?
